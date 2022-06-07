de76077091f28e00aef2940cfec549121e4516e4
ruiqi.he <ruiqi.he@yaduo.com>
2022-06-07 22:51:40 +0800

    init

---
+++ src/main/java/com/open/eazyregistrationcenter/EazyRegistrationCenterApplication.java
@@ -0,0 +1,13 @@
+package com.open.eazyregistrationcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyRegistrationCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazyRegistrationCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/open/eazyregistrationcenter/EazyRegistrationCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.eazyregistrationcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyRegistrationCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
